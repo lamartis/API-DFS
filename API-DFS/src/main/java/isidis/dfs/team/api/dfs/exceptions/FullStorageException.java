02e78d813f7bfc0ef628e0b7f7b71ce27fbb368e
root <root@dfs-one.(none)>
2014-01-28 15:54:14 +0100

    Added forgotten exception classes

---
+++ API-DFS/src/main/java/isidis/dfs/team/api/dfs/exceptions/FullStorageException.java
@@ -0,0 +1,10 @@
+package isidis.dfs.team.api.dfs.exceptions;
+
+public class FullStorageException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ API-DFS/src/main/java/isidis/dfs/team/api/dfs/exceptions/SystemUserPermissionException.java
@@ -0,0 +1,12 @@
+package isidis.dfs.team.api.dfs.exceptions;
+
+import org.apache.hadoop.security.AccessControlException;
+
+public class SystemUserPermissionException extends AccessControlException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
