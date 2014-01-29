4789cc88bad41afd040904dd25be778e170c32ae
lamarti saad <lamartisaad@live.fr>
2014-01-30 00:17:56 +0100

    Added test integration

--- API-DFS/src/test/java/isidis/dfs/team/API_DFS/ItHDFSApi.java
+++ API-DFS/src/test/java/isidis/dfs/team/API_DFS/ItHDFSApi.java
@@ -1,48 +1,90 @@
 package isidis.dfs.team.API_DFS;
 
 import static org.junit.Assert.*;
-
 import java.net.URISyntaxException;
-
 import isidis.dfs.team.api.dfs.exceptions.*;
-
 import org.apache.hadoop.fs.FileAlreadyExistsException;
 import org.junit.Assert;
 import org.junit.Test;
-
 import isidis.dfs.team.api.dfs.implementation.API_HDFS_Impl;
 import isidis.dfs.team.api.dfs.interfaces.API_HDFS;
 
 public class ItHDFSApi {
+    
+    public static final String hdfsURL = "hdfs://192.168.0.41:9000";
+    public static final String systemUserName = "hduser";
+    public static final String fileName = "/user/fileForIntegration7";
+    public static final String texte = "This is a test";
+    
+    @Test
+    public void testAPI_HDFS_Impl() {
+        
+        API_HDFS api = null;
+        try {
+            api = new API_HDFS_Impl(hdfsURL, systemUserName);
+        } catch (URISyntaxException e1) {
+            Assert.fail("URI Syntax Error");
+        }
 
-	@Test
-	public void testAPI_HDFS_Impl() throws URISyntaxException {
-		String hdfsURL = "hdfs://192.168.0.41:9000/";
-		String systemUserName = "hduser";
-		API_HDFS IT_api = new API_HDFS_Impl(hdfsURL, systemUserName);
-		try {
-			
-			IT_api.writeFile("This is a test".getBytes(), "test.txt");
-			IT_api.readFile("test.txt");
-			IT_api.deleteFile("test.txt");
-		} catch (SystemUserPermissionException e) {
-			Assert.fail("SystemUserPermissionException");
-		} catch (FileAlreadyExistsException e) {
-			Assert.fail("FileAlreadyExistsException");
-		} catch (EndpointNotReacheableException e) {
-			Assert.fail("EndpointNotReacheableException");
-		} catch (FileNotFoundException e) {
-			Assert.fail("FileNotFoundException");
-		}
-		try {
-			IT_api.readFile("test.txt");
-		} catch (SystemUserPermissionException e) {
-			Assert.fail("SystemUserPermissionException");
-		} catch (FileNotFoundException e) {
-			Assert.fail("Integration work fine");
-		} catch (EndpointNotReacheableException e) {
-			Assert.fail("EndpointNotReacheableException");
-		}
-	}
+        /***
+         * Testing WriteFile method
+         **/
+        try {
+            api.writeFile(texte.getBytes(), fileName);
+        } catch (SystemUserPermissionException e) {
+            Assert.fail("permission error");
+        } catch (FileAlreadyExistsException e) {
+            Assert.fail("file already exist error");
+        } catch (EndpointNotReacheableException e) {
+            Assert.fail("Endpoint not reachable");
+        }
+        
+        /***
+         * Testing ReadFile method
+         **/
+
+        try {
+            assertTrue(texte.equals(new String(api.readFile(fileName)).trim()));
+        } catch (SystemUserPermissionException e1) {
+            Assert.fail("permession error");
+        } catch (FileNotFoundException e1) {
+            Assert.fail("file not found");
+        } catch (EndpointNotReacheableException e1) {
+            Assert.fail("endpoint not reachbear");
+        }
+        
+
+        /***
+         * Testing deleteFile method
+         */
+
+        try {
+            api.deleteFile(fileName);
+        } catch (SystemUserPermissionException e) {
+            Assert.fail("permission error");
+        } catch (FileNotFoundException e) {
+            Assert.fail("file not found");
+        } catch (EndpointNotReacheableException e) {
+            Assert.fail("Endpoint not reachable");
+        }
+        
+        
+        /***
+         * Testing ReadFile method
+         **/
+
+        try {
+            assertEquals(texte,(new String(api.readFile(fileName))));
+            Assert.fail("File have to not exist [Exception not thrown]");
+        } catch (SystemUserPermissionException e1) {
+            Assert.fail("permession error");
+        } catch (FileNotFoundException e1) {
+            System.out.println("Exception is throwen");
+        } catch (EndpointNotReacheableException e1) {
+            Assert.fail("endpoint not reachbear");
+        }
+        
+        
+    }
 
 }
