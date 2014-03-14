8ce40de4146f76b83caf4858c646fb6e9a038633
DFS-One <dfs-one@dfs-one.(none)>
2014-03-14 19:56:39 +0100

    Reconfigured size split to 100Ko

--- common/src/main/java/isidis/dfs/team/api/dfs/common/implementation/MyHdfsClient.java
+++ common/src/main/java/isidis/dfs/team/api/dfs/common/implementation/MyHdfsClient.java
@@ -23,9 +23,9 @@ public class MyHdfsClient {
 	
 	public static MyHdfsClient myHdfsClient = null;
 	/**
-	 * Blocks will be splited into 200Ko.
+	 * Blocks will be splited into 100Ko.
 	 */
-	public Long Ko = (long) 200;
+	public Long Ko = (long) 100;
 	public Long blockSizeInOctet = Ko * 1024;
 
 	public static String hdfsURL = null;
